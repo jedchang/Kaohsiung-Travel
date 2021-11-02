bb9e1b67fe7608755d0b04bbdb793f01fae3f28f
Jed Chang <march.24@gmail.com>
2021-11-02 10:01:45 +0800

    [EDIT] 修正 API 無法使用 & 增加 favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,119 +1,115 @@
-(function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open(
-    'get',
-    'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97',
-    true
-  );
-  xhr.send(null);
-  xhr.onload = function() {
-    var loader = document.querySelector('.loader');
+;(function () {
+  var xhr = new XMLHttpRequest()
+  xhr.open('get', 'https://raw.githubusercontent.com/jedchang/Kaohsiung-Travel/master/js/data.json', true)
+  xhr.send(null)
+  xhr.onload = function () {
+    var loader = document.querySelector('.loader')
     if (xhr.readyState == 4 && xhr.status == 200) {
-      loader.style.display = 'none';
-      console.log('成功讀取資料');
-      xhrData();
+      loader.style.display = 'none'
+      console.log('成功讀取資料')
+      xhrData()
     } else {
-      loader.style.display = 'block';
-      console.log('資料錯誤!!');
+      loader.style.display = 'block'
+      console.log('資料錯誤!!')
     }
-  };
+  }
 
   function xhrData() {
-    var dataStr = JSON.parse(xhr.responseText);
-    var data = dataStr.result.records;
+    var dataStr = JSON.parse(xhr.responseText)
+    var data = dataStr.result.records
 
-    var btnZone = document.querySelector('.hot-zone');
-    var infoContent = document.querySelector('.info');
-    var zoneTitleName = document.querySelector('.zone-name');
-    var pagination = document.querySelector('.pagination');
-    var pageNumber = document.querySelector('#pageNum');
-    var selection = document.querySelector('#zone');
+    var btnZone = document.querySelector('.hot-zone')
+    var infoContent = document.querySelector('.info')
+    var zoneTitleName = document.querySelector('.zone-name')
+    var pagination = document.querySelector('.pagination')
+    var pageNumber = document.querySelector('#pageNum')
+    var selection = document.querySelector('#zone')
 
-    var selectList = [];
+    var selectList = []
     for (var i = 0; i < data.length; i++) {
-      var selectZone = data[i].Zone;
-      selectList.push(selectZone);
+      var selectZone = data[i].Zone
+      selectList.push(selectZone)
     }
 
-    var selected = [];
-    selectList.forEach(function(value) {
+    var selected = []
+    selectList.forEach(function (value) {
       if (selected.indexOf(value) == -1) {
-        selected.push(value);
+        selected.push(value)
       }
-    });
+    })
 
     for (var i = 0; i < selected.length; i++) {
-      var option = document.createElement('option');
-      option.textContent = selected[i];
-      selection.appendChild(option);
+      var option = document.createElement('option')
+      option.textContent = selected[i]
+      selection.appendChild(option)
     }
 
-    btnZone.addEventListener('click', hotZone);
-    pagination.addEventListener('click', paginationClick);
-    selection.addEventListener('change', selectedList);
+    btnZone.addEventListener('click', hotZone)
+    pagination.addEventListener('click', paginationClick)
+    selection.addEventListener('change', selectedList)
 
     function queryZone(zoneName) {
-      selected = [];
+      selected = []
       for (var i = 0; i < data.length; i++) {
         if (data[i].Zone === zoneName) {
-          selected.push(data[i]);
+          selected.push(data[i])
         }
       }
     }
 
     function hotZone(e) {
       if (e.target.nodeName == 'BUTTON') {
-        queryZone(e.target.textContent);
-        renderContent(1);
-        console.log(selected);
+        queryZone(e.target.textContent)
+        renderContent(1)
+        console.log(selected)
       }
     }
 
     function selectedList(e) {
-      var value = e.target.value;
-      queryZone(value);
-      renderContent(1);
+      var value = e.target.value
+      queryZone(value)
+      renderContent(1)
     }
 
-    var totalPage;
-    var totalItem;
-    var currentPage;
-    var perPage = 4;
+    var totalPage
+    var totalItem
+    var currentPage
+    var perPage = 4
 
     function renderContent(page) {
-      $('html,body').animate({ scrollTop: $('.zone-name').offset().top }, 1000);
+      $('html,body').animate({ scrollTop: $('.zone-name').offset().top }, 1000)
 
-      totalItem = selected.length;
-      zoneTitleName.textContent = selected[0].Zone;
+      totalItem = selected.length
+      zoneTitleName.textContent = selected[0].Zone
 
-      totalPage = Math.ceil(totalItem / perPage);
-      let startItem;
-      let endItem;
+      totalPage = Math.ceil(totalItem / perPage)
+      let startItem
+      let endItem
 
       if (page == totalPage) {
-        var remainingItem = totalItem - totalPage * perPage;
+        var remainingItem = totalItem - totalPage * perPage
         if (remainingItem == 0) {
-          startItem = (totalPage - 1) * perPage;
-          endItem = totalItem;
+          startItem = (totalPage - 1) * perPage
+          endItem = totalItem
         } else {
-          startItem = (totalPage - 1) * perPage;
-          endItem = totalItem;
+          startItem = (totalPage - 1) * perPage
+          endItem = totalItem
         }
       } else {
-        startItem = perPage * (page - 1);
-        endItem = page * perPage;
+        startItem = perPage * (page - 1)
+        endItem = page * perPage
       }
       // console.log('startItem:' + startItem, 'endItem:' + endItem, 'totalPage:' + totalPage);
 
-      var tempHTML = '';
+      var tempHTML = ''
       for (var i = startItem; i < endItem; i++) {
-        var name = selected[i].Name;
-        var zone = selected[i].Zone;
-        var openTime = selected[i].Opentime;
-        var add = selected[i].Add;
-        var tel = selected[i].Tel;
-        var ticket = selected[i].Ticketinfo;
-        var picture = selected[i].Picture1;
+        var name = selected[i].Name
+        var zone = selected[i].Zone
+        var openTime = selected[i].Opentime
+        var add = selected[i].Add
+        var tel = selected[i].Tel
+        var ticket = selected[i].Ticketinfo
+        var picture = selected[i].Picture1
         if (ticket == '') {
           tempHTML +=
             '<li class="item"><div class="image-wrap"><div class="photo"><img src="' +
@@ -128,7 +124,7 @@
             add +
             '</p><p class="tel"><i class="fas fa-mobile-alt"></i>' +
             tel +
-            '</p></div></li>';
+            '</p></div></li>'
         } else {
           tempHTML +=
             '<li class="item"><div class="image-wrap"><div class="photo"><img src="' +
@@ -143,76 +139,76 @@
             add +
             '</p><p class="tel"><i class="fas fa-mobile-alt"></i>' +
             tel +
-            '</p></div></li>';
+            '</p></div></li>'
         }
       }
-      infoContent.innerHTML = tempHTML;
-      currentPage = page;
-      renderPage();
-      selectedPageBtn();
-      selectedPageBtn();
+      infoContent.innerHTML = tempHTML
+      currentPage = page
+      renderPage()
+      selectedPageBtn()
+      selectedPageBtn()
     }
 
     function renderPage() {
       if (selected.length <= 0 || selected.length <= 6) {
-        pagination.style.display = 'none';
+        pagination.style.display = 'none'
       } else {
-        pagination.style.display = 'block';
+        pagination.style.display = 'block'
         if (totalPage > 0) {
-          var tempNum = '';
+          var tempNum = ''
           for (var i = 1; i < totalPage + 1; i++) {
-            tempNum += '<a href="javascript:;" class="click-page-num">' + i + '</a>';
+            tempNum += '<a href="javascript:;" class="click-page-num">' + i + '</a>'
           }
-          pageNumber.innerHTML = tempNum;
+          pageNumber.innerHTML = tempNum
         }
       }
     }
 
     function selectedPageBtn() {
-      var clickPageNum = document.querySelectorAll('.click-page-num');
+      var clickPageNum = document.querySelectorAll('.click-page-num')
       for (var i = 0; i < clickPageNum.length; i++) {
         if (i == currentPage - 1) {
-          clickPageNum[i].classList.add('current');
+          clickPageNum[i].classList.add('current')
         } else {
-          clickPageNum[i].classList.remove('current');
+          clickPageNum[i].classList.remove('current')
         }
       }
 
-      var btnStatus = document.querySelectorAll('.pageBtn a');
+      var btnStatus = document.querySelectorAll('.pageBtn a')
       if (currentPage == 1) {
-        btnStatus[0].classList.add('disabled');
+        btnStatus[0].classList.add('disabled')
       } else {
-        btnStatus[0].classList.remove('disabled');
+        btnStatus[0].classList.remove('disabled')
       }
       if (currentPage == totalPage) {
-        btnStatus[1].classList.add('disabled');
+        btnStatus[1].classList.add('disabled')
       } else {
-        btnStatus[1].classList.remove('disabled');
+        btnStatus[1].classList.remove('disabled')
       }
     }
     function paginationClick(e) {
       if (e.target.nodeName == 'A') {
-        pageNum = Number(e.target.dataset.num);
+        pageNum = Number(e.target.dataset.num)
         if (pageNum == 1) {
-          currentPage++;
-          renderContent(currentPage);
+          currentPage++
+          renderContent(currentPage)
         } else if (pageNum == 0) {
-          currentPage--;
-          renderContent(currentPage);
+          currentPage--
+          renderContent(currentPage)
         } else {
-          e.target.textContent;
-          currentPage = e.target.textContent;
-          renderContent(currentPage);
+          e.target.textContent
+          currentPage = e.target.textContent
+          renderContent(currentPage)
         }
         // console.log(currentPage, pageNum);
       }
     }
 
-    (function init() {
-      var initTemp = '';
+    ;(function init() {
+      var initTemp = ''
       for (var i = 0; i < data.length; i++) {
         if (data[i].Zone == '內門區') {
-          zoneTitleName.textContent = '內門區';
+          zoneTitleName.textContent = '內門區'
           initTemp +=
             '<li class="item"><div class="image-wrap"><div class="photo"><img src="' +
             data[i].Picture1 +
@@ -226,40 +222,38 @@
             data[i].Add +
             '</p><p class="tel"><i class="fas fa-mobile-alt"></i>' +
             data[i].Tel +
-            '</p></div></li>';
+            '</p></div></li>'
         }
-        infoContent.innerHTML = initTemp;
+        infoContent.innerHTML = initTemp
       }
-    })();
+    })()
 
-    $('#goTop').click(function() {
+    $('#goTop').click(function () {
       $('html,body').animate(
         {
-          scrollTop: 0
+          scrollTop: 0,
         },
         800
-      );
-      return false;
-    });
+      )
+      return false
+    })
 
-    $('#goDown').click(function() {
+    $('#goDown').click(function () {
       $('html,body').animate(
         {
-          scrollTop: $('#popular').offset().top
+          scrollTop: $('#popular').offset().top,
         },
         800
-      );
-      return false;
-    });
+      )
+      return false
+    })
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       if ($(this).scrollTop() > 300) {
-        $('#goTop').fadeIn('fast');
+        $('#goTop').fadeIn('fast')
       } else {
-        $('#goTop')
-          .stop()
-          .fadeOut('fast');
+        $('#goTop').stop().fadeOut('fast')
       }
-    });
+    })
   }
-})();
+})()
